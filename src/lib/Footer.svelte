7ef638b43e7d11920b8a54f46ad86ed8fc4e0eaf
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-17 07:26:03 -0600

    trigger site re-deploy on contentful webhook
    remove open source note from footer, update deps

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,21 +3,21 @@
   import Social from './Social.svelte'
   import type { SocialNetwork } from './types'
 
+  export let social: Record<SocialNetwork, string>
+  export let links: Record<string, string>
+
+  const style = `height: 1em; vertical-align: -3pt; padding-right: 2pt;`
   const icon_map = {
     Datenschutz: `ic:round-privacy-tip`,
     Impressum: `octicon:law`,
     Kontakt: `ic:round-alternate-email`,
     Satzung: `ic:round-text-snippet`,
   }
-
-  export let social: Record<SocialNetwork, string>
-  export let links: Record<string, string>
-  const style = `height: 1em; vertical-align: -3pt; padding-right: 2pt;`
 </script>
 
 <footer>
   <div class="copyright">
-    <img src="/favicon.svg" alt="Logo" height="60" />
+    <img src="/favicon.svg" alt="Logo" height="50" />
     <span>© {new Date().getFullYear()} Afara e.V.</span>
   </div>
   <Social {social} style="margin-top: 1ex;" />
@@ -29,25 +29,19 @@
       </a>
     {/each}
   </div>
-  <span>
-    Diese Seite ist
-    <a href="https://github.com/janosh/afara">
-      <Icon icon="mdi:open-source-initiative" inline />
-      open source
-    </a>
-  </span>
 </footer>
 
 <style>
   footer {
     padding: 3em calc(1em + 3vw);
     text-align: center;
-    display: grid;
-    gap: 2em;
-    place-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: 3em;
     background: var(--dark-gray);
     color: white;
-    padding-inline: max(1em, calc(50vw - 30em));
+    place-content: center;
+    place-items: center;
   }
   footer :global(a) {
     color: var(--light-green);
@@ -60,20 +54,7 @@
     display: flex;
     gap: 3pt 1em;
     flex-wrap: wrap;
-    justify-content: center;
-  }
-  @media (max-width: 700px) {
-    footer div.copyright {
-      display: contents;
-    }
-  }
-  @media (min-width: 701px) {
-    footer {
-      place-content: center;
-      grid-template-columns: 1fr 1fr;
-    }
-    footer div.copyright {
-      place-items: center;
-    }
+    place-content: center;
+    place-items: center;
   }
 </style>
